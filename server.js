a8427942e84afbc4d2e08887ecf40e6972060d49
Elisei <strashevsky.elisey@gmail.com>
2024-02-11 18:50:30 +0300

    first

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+http.createServer(function(req, res){
+	res.write("this is backend!");
+	res.end();
+
+}).listen(5000);
+
+console.log("Backend server running at port 5000");
