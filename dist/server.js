5c50ffd95e93baca04b2744fffcd14edc446f728
Elisei <strashevsky.elisey@gmail.com>
2024-02-13 13:45:07 +0300

    router test

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,15 @@
 import express from "express";
-import router from "./router.js";
 const app = express();
+const router = express.Router();
 app.get("/", (req, res) => {
     console.log("hello from express");
     res.status(200);
     res.json({ message: "hello WTF" });
 });
-app.use("/router", router);
+router.get("/users", (_req, res) => {
+    res.json({ message: "YO WHATSAP" });
+});
+app.use("/", router);
 app.listen(5000, () => {
     console.log("5000 port started");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import router from "./router.js";
 const app = express();
+const router = express.Router();
 
 app.get("/", (req, res) => {
   console.log("hello from express");
@@ -8,7 +8,11 @@ app.get("/", (req, res) => {
   res.json({ message: "hello WTF" });
 });
 
-app.use("/router", router);
+router.get("/users", (_req, res) => {
+  res.json({ message: "YO WHATSAP" });
+});
+
+app.use("/", router);
 
 app.listen(5000, () => {
   console.log("5000 port started");
