6c1b9d2586f30cb67c927c6549f97527bae1aec1
Elisei <strashevsky.elisey@gmail.com>
2024-02-13 11:47:56 +0300

    router test

---
+++ dist/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get("/", function (req, res) {
+    console.log("hello from express");
+    res.status(200);
+    res.json({ message: "hello" });
+});
+app.listen(5000, function () {
+    console.log("5000 port started");
+});
+//# sourceMappingURL=server.js.map
