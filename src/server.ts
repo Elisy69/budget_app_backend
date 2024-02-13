2db11db8c016bb47c257b83a4b44ff40f576fe7f
Elisei <strashevsky.elisey@gmail.com>
2024-02-13 10:57:53 +0300

    router test

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import express from "express";
 import router from "./router";
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("hello from express");
+  res.status(200);
+  res.json({ message: "hello" });
+});
+
 app.use("/", router);
 
 app.listen(5000, () => {
