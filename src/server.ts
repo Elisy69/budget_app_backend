d7dab372e87a40a0779326617419a30915afe6eb
Elisei <strashevsky.elisey@gmail.com>
2024-02-13 13:51:47 +0300

    router test

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import express from "express";
 const app = express();
 const router = express.Router();
 
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json({ message: "hello AFSASDFAFS" });
+});
+
 router.get("/users", (_req, res) => {
+  res.status(200);
   res.json({ message: "YO WHATSAP" });
 });
 
-app.use("/", router);
+app.use("/api", router);
 
 app.listen(5000, () => {
   console.log("5000 port started");
