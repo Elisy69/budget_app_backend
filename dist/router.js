f4e4374d5d68131efabfa4606eddb921f35f342a
Elisei <strashevsky.elisey@gmail.com>
2024-02-13 15:00:41 +0300

    router test

--- dist/router.js
+++ dist/router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 router.get("/", (_req, res) => {
-    res.json({ message: "SO THIS IS USERS" });
+    res.json({ message: "users home page" });
 });
 export default router;
 //# sourceMappingURL=router.js.map
