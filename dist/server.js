2249c2000ae41079d614f323d1b37e3008fa8134
Elisei <strashevsky.elisey@gmail.com>
2024-02-14 17:43:35 +0300

    auth testing

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import router from "./router.js";
 import morgan from "morgan";
+import { signin } from "./handlers/users.js";
+import { createNewUser } from "./handlers/users.js";
+import { protect } from "./modules/auth.js";
 import * as dotenv from "dotenv";
 dotenv.config();
+const PORT = 5000;
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
@@ -10,8 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.sendStatus(404);
 });
-app.use("/users", router);
-app.listen(5000, () => {
-    console.log("5000 port started");
+app.use("/users", protect, router);
+app.post("/newuser", createNewUser);
+app.post("/signin", signin);
+app.listen(PORT, () => {
+    console.log(`Server on ${PORT} port started`);
 });
 //# sourceMappingURL=server.js.map
